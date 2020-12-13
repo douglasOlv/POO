e9080bdee7d5984007af4c392755ba9c805662cd
Douglas Oliveira <douglas4030@gmail.com>
2020-12-12 21:07:45 -0300

    concepts

---
+++ 01-concepts/Pessoa.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Pessoa 
+{
+    // Attributes
+    public string name;
+    public int age;
+
+    // Methods
+    public void message()
+    {
+        Console.WriteLine("Hello "+name+" you have "+age+" years old" );
+    }
+}
---
+++ 01-concepts/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01_concepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate Object
+            Pessoa obj = new Pessoa();
+            obj.name = "Douglas";
+            obj.age = 26;
+            obj.message();
+        }
+    }
+}
